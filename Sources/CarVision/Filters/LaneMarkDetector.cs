fb49e95b6b635747b9b10a24a0e06844852d807c
Piotr Grodek <ipc0nfg@gmail.com>
2012-11-27 13:44:34 +0100

    Added filter for perspective correction and lane mark detector filter.
    Added two interesting papers.

---
+++ Sources/CarVision/Filters/LaneMarkDetector.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+namespace CarVision.Filters
+{
+    /// <summary>
+    /// This class finds pixel which may be white road lane.
+    /// </summary>
+    class LaneMarkDetector : ThreadSupplier<Image<Gray, Byte>, Image<Gray, Byte>>
+    {
+        private Supplier<Image<Gray, Byte>> supplier;
+        private int tau;
+
+        public int Tau
+        {
+            get { return tau;  }
+            set { tau = value < 1 ? 1 : value; }
+        }
+
+        private void DetectLaneMark(Image<Gray, Byte> img)
+        {
+            Image<Gray, Byte> dst = new Image<Gray, byte>(img.Width, img.Height);
+
+            double aux;
+            int i, j;
+            int w = img.Width - tau - 1;
+            for (j = 0; j < img.Height; ++j)
+            {
+                for (i = tau; i < w; ++i)
+                {
+                    aux = 2.0 * img[j, i].Intensity;
+                    aux -= img[j, i - tau].Intensity;
+                    aux -= img[j, i + tau].Intensity;
+                        
+                    aux -= Math.Abs(img[j, i - tau].Intensity - img[j, i + tau].Intensity);
+
+                    if (aux > 255.0) aux = 255.0;
+                    else if (aux < 0.0) aux = 0.0;
+
+                    dst[j, i] = new Gray(aux);
+                }
+            }
+
+            LastResult = dst;
+            PostComplete();
+        }
+
+        public LaneMarkDetector(Supplier<Image<Gray, Byte>> supplier_)
+        {
+            supplier = supplier_;
+            supplier.ResultReady += MaterialReady;
+
+            Tau = 5;
+
+            Process += DetectLaneMark;
+        }
+
+    }
+}
--- Sources/CarVision/ViewForm.cs
+++ Sources/CarVision/ViewForm.cs
@@ -18,6 +18,8 @@ namespace CarVision
     {
         VideoSource videoSource;
         Smoothener smoothener;
+        LaneMarkDetector laneDetector;
+        PerspectiveCorrection perpCorr;
 
         private void DisplayVideo(object sender, CarVision.ResultReadyEventArgs e)
         {
@@ -25,6 +27,8 @@ namespace CarVision
             if (sender == videoSource)
                 imgBox = imgVideoSource;
             else if (sender == smoothener)
+                imgBox = imgSmoothener;
+            else if (sender == laneDetector || sender == perpCorr)
                 imgBox = imgSmoothener;
             if (imgBox == null)
                 throw new InvalidOperationException("No receiver registered");
@@ -39,8 +43,19 @@ namespace CarVision
             videoSource = new VideoSource();
             videoSource.ResultReady += DisplayVideo;
 
-            smoothener = new Smoothener(videoSource);
-            smoothener.ResultReady += DisplayVideo;
+            //smoothener = new Smoothener(videoSource);
+            //smoothener.ResultReady += DisplayVideo;
+
+            laneDetector = new LaneMarkDetector(videoSource);
+            //laneDetector.ResultReady += perpCorr.;
+
+            PointF[] src = { new PointF(0,320), new PointF(320, 320), new PointF(150, 320-240), new PointF(75,320-240) };
+            PointF[] dst = { new PointF(0,320), new PointF(320, 320), new PointF(320, 320-240), new PointF(0,320-240) };
+            
+            perpCorr = new PerspectiveCorrection(laneDetector, src, dst);
+            perpCorr.ResultReady += DisplayVideo;
+
+
             
         }
 
