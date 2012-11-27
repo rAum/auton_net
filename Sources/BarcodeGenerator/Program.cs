b0f3a000caeb571885c4bd8d7cfc5dce4322872d
Michal Flendrich <m.flendrich@gmail.com>
2012-11-27 01:09:55 +0100

    barcode generator main method cleaned up

--- Sources/BarcodeGenerator/Program.cs
+++ Sources/BarcodeGenerator/Program.cs
@@ -16,34 +16,6 @@ namespace BarcodeGenerator
         [STAThread]
         static void Main()
         {
-            int count = 0;
-
-            LinkedList<int> nums = new LinkedList<int>();
-
-            for (int num = 0; num < 1024; ++num)
-            {
-                string bin = Convert.ToString(num, 2);
-                for (int i = bin.Length; i <= 10; ++i)
-                    bin = "0" + bin;
-
-                if (bin.IndexOf("0000") == -1 && bin.IndexOf("1111") == -1 
-                    && !bin.StartsWith("111")
-                    && !bin.EndsWith("00") && !bin.EndsWith("11"))
-                {
-                    Console.WriteLine("{0} {1}", num, bin);
-                    nums.AddLast(num);
-                }
-            }
-
-            Console.WriteLine("Found: {0}", nums.Count);
-
-            Console.Write("{ ");
-            foreach (int num in nums)
-                Console.Write("{0}, ", num);
-            Console.WriteLine("}");
-
-            Console.ReadKey();
-
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new GeneratorForm());
