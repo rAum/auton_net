4c95e4d5a7dbccdec7c13c84abb7d1076aef4256
Piotr Grodek <ipc0nfg@gmail.com>
2013-03-07 11:37:49 +0100

    implementing bezier for RANSAC
    bezier curve may behave better than quadratic equation.
    Also Catmull-Rom will be next.

--- Sources/VisionFilters/RANSAC/Functions.cs
+++ Sources/VisionFilters/RANSAC/Functions.cs
@@ -6,6 +6,111 @@ using System.Drawing;
 
 namespace RANSAC.Functions
 {
+    public class Vec2
+    {
+        double[] p = { 0, 0 };
+
+        public double X { get { return p[0]; } set { p[0] = value; } }
+        public double Y { get { return p[1]; } set { p[1] = value; } }
+
+        public Vec2()
+        {
+        }
+
+        public Vec2(double x, double y)
+        {
+            p[0] = x; p[1] = y;
+        }
+
+        public Vec2(int x, int y)
+        {
+            p[0] = x; p[1] = y;
+        }
+
+        public Vec2(Point pt)
+        {
+            p[0] = pt.X;
+            p[1] = pt.Y;
+        }
+
+        static Vec2 operator -(Vec2 a, Vec2 b)
+        {
+            return new Vec2(a.p[0] - b.p[0], a.p[1] - b.p[1]);
+        }
+
+        static Vec2 operator +(Vec2 a, Vec2 b)
+        {
+            return new Vec2(a.p[0] + b.p[0], a.p[1] + b.p[1]);
+        }
+
+        static Vec2 operator *(double t, Vec2 a)
+        {
+            return new Vec2(t * a.p[0], t * a.p[1]);
+        }
+
+        public Point ToPoint()
+        {
+            return new Point((int)Math.Round(p[0]), (int)Math.Round(p[1]));
+        }
+    }
+
+    public class Bezier
+    {
+        Vec2[] points;
+        double minY, maxY, rangeY;
+
+        public Bezier()
+        {
+        }
+
+        public static Bezier fit(List<Point> points_)
+        {
+            Bezier b = new Bezier();
+            
+            b.points = (from p in points_
+                        orderby p.Y // sorting unneeded?
+                        select new Vec2(p)).ToArray();
+            
+            b.minY = b.points[0].Y;
+            b.maxY = b.points[b.points.Length - 1].Y;
+            b.rangeY = b.maxY - b.minY;
+
+            return b;
+        }
+
+        public Vec2 at(int y)
+        {
+            double t = (y - minY) / rangeY;
+            return castelijou(t);
+        }
+
+        private Vec2 castelijou(double t)
+        {
+            Vec2[] c = points.ToArray();
+            int n = c.Length - 1;
+            for (int i = 0; i < c.Length; ++i)
+            {
+                for (int j = 0; j < n; ++j)
+                {
+                    c[j] = c[j] + t * (c[j + 1] - c[j]);
+                }
+                --n;
+            }
+
+            return c[0];
+        }
+
+        public string ToString()
+        {
+            StringBuilder bd = new StringBuilder();
+            bd.Append('[');
+            foreach (var p in points)
+                bd.AppendFormat(" ({0}, {1}),", p.X, p.Y);
+            bd.Append("]\n");
+            return bd.ToString();
+        }
+    }
+
     public class Parabola
     {
         public double a { get; set; }
