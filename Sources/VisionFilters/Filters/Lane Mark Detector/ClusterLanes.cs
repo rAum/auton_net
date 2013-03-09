202f8f397755bd2286ddb11d04932f7f0f20a886
Piotr Grodek <ipc0nfg@gmail.com>
2013-03-09 07:43:01 +0100

    road center estimation - fix

--- Sources/VisionFilters/Filters/Lane Mark Detector/ClusterLanes.cs
+++ Sources/VisionFilters/Filters/Lane Mark Detector/ClusterLanes.cs
@@ -203,7 +203,7 @@ namespace VisionFilters.Filters.Lane_Mark_Detector
 
                 // reestimate road center
                 double new_road_width = ((rightLane.c - roadCenter.c) + (roadCenter.c - leftLane.c)) * 0.5 * 0.1 + roadCenterDistAvg * 0.9;
-                roadCenterDistAvg = Math.Max(Math.Min(new_road_width, ROAD_CENTER_MAX), ROAD_CENTER_MAX);
+                roadCenterDistAvg = Math.Max(Math.Min(new_road_width, ROAD_CENTER_MAX), ROAD_CENTER_MIN);
             }
             else if (leftLane != null) // check if this is really a left lane
             {
