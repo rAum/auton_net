babb5618507f2242e08ed08454194a7dc665ad45
Spawek <spawnerer@gmail.com>
2013-04-18 17:33:00 +0200

    improved StatsCollector to work all the time and create files for every run

--- Sources/Helpers/StatsCollector.cs
+++ Sources/Helpers/StatsCollector.cs
@@ -8,60 +8,89 @@ namespace Helpers
 {
     public class StatsCollector
     {
-        private IDictionary<string, List<double>> dict = new Dictionary<string, List<double>>();
+        private IDictionary<string, LinkedList<double>> dict = new Dictionary<string, LinkedList<double>>();
+        private StreamWriter sw;
+        private bool keysWritten = false;
+        private const int NEW_STATS_NO_BETW_WRITE_TO_FILE = 1000;
+        private string outFileName;
+        private Object FileLock = new Object();
+
+        public StatsCollector()
+        {
+            outFileName = String.Format("stats_{0}.txt", DateTime.Now.ToString("d_M_yyyy__HH_mm_ss"));
+            if (File.Exists(outFileName))
+            { //but this file shouldn't exist anyway
+                File.Delete(outFileName);
+            }
+
+            sw = new StreamWriter(outFileName);
+        }
+
+        private int putsFromLastSave = 0;
         public void PutNewStat(string variable, double value)
         {
             //create vector for key if not exists
             if (!dict.ContainsKey(variable))
             {
-                dict[variable] = new List<double>();
+                if (keysWritten)
+                    throw new ApplicationException("You can't add new keys after first save to file");
+
+                dict[variable] = new LinkedList<double>();
             }
 
-            dict[variable].Add(value);
+            dict[variable].AddLast(value);
+
+            if (++putsFromLastSave > NEW_STATS_NO_BETW_WRITE_TO_FILE)
+            {
+                WriteStatsToFile();
+                putsFromLastSave = 0;
+            }
         }
 
-        public void WriteStatsToFile(string fileName)
+        public void WriteStatsToFile()
         {
-            if(File.Exists(fileName))
-            {
-                File.Delete(fileName);
-            }
+            Logger.Log(this, String.Format("Starting to save new stats: {0}", outFileName));
 
-            using (StreamWriter sw = new StreamWriter(fileName))
+            lock (FileLock)
             {
                 //write keys
-                foreach (string key in dict.Keys)
+                if (!keysWritten)
                 {
-                    sw.Write("{0}; ", key);
+                    foreach (string key in dict.Keys)
+                    {
+                        sw.Write("{0}; ", key);
+                    }
+                    sw.WriteLine();
+                    keysWritten = true;
                 }
-                sw.WriteLine();
 
                 //write values
                 bool somethingWasWritten = true;
-                int i = 0;
                 while (somethingWasWritten)
                 {
-                    somethingWasWritten = false; 
+                    somethingWasWritten = false;
 
-                    foreach(string key in dict.Keys)
+                    foreach (string key in dict.Keys)
                     {
-                        if (dict[key].Count > i)
+                        if (dict[key].Count > 0)
                         {
-                            sw.Write("{0}; ", dict[key][i]);
+                            sw.Write("{0}; ", dict[key].First());
+                            dict[key].RemoveFirst();
                             somethingWasWritten = true;
                         }
                         else
                         {
                             sw.Write(";");
                         }
+
+                        if (key == dict.Keys.Last())
+                        {
+                            sw.WriteLine();
+                        }
                     }
-                    sw.WriteLine();
-
-                    i++;
                 }
-
-                sw.Close();
             }
+            Logger.Log(this, String.Format("Saving to file ended successfully: {0}", outFileName));
         }
 
     }
