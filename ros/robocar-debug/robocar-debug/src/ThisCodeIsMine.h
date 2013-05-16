055eec6e618957b7d6b0bd921736e48c0b6ab5b5
dextero <hukutizuviki@gmail.com>
2013-05-16 22:22:41 +0200

    Moving debug tool to a branch

--- ros/robocar-debug/robocar-debug/src/ThisCodeIsMine.h
+++
@@ -1,3 +0,0 @@
-// dex: Qt and ROS code generates so much warnings it's driving me crazy
-// re-enables warnings
-#pragma clang diagnostic pop
--- ros/robocar-debug/robocar-debug/src/ThisCodeIsNotMine.h
+++
@@ -1,5 +0,0 @@
-// dex: Qt and ROS code generate so much warnings it's driving me crazy
-// suppresses warnings
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wunused-parameter"
-#pragma clang diagnostic ignored "-Wuninitialized"
